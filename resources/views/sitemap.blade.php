e230219c03645e9beed06608b5818b7bf542a1b9
Damien Jolly <DamienJolly@hotmail.com>
2026-03-12 17:59:02 +0000

    sitemap fix.

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -1,46 +1,21 @@
-<?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>'; ?>
+{!! '<?xml version="1.0" encoding="UTF-8"?>' !!}
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-
-  <url>
-    <loc>https://spicnspansuffolk.co.uk/</loc>
-    <changefreq>weekly</changefreq>
-    <priority>1.0</priority>
-  </url>
-
-  <url>
-    <loc>https://spicnspansuffolk.co.uk/services</loc>
-    <changefreq>monthly</changefreq>
-    <priority>0.9</priority>
-  </url>
-
-  <url>
-    <loc>https://spicnspansuffolk.co.uk/services/house-cleaning</loc>
-    <changefreq>monthly</changefreq>
-    <priority>0.8</priority>
-  </url>
-
-  <url>
-    <loc>https://spicnspansuffolk.co.uk/services/commercial-cleaning</loc>
-    <changefreq>monthly</changefreq>
-    <priority>0.8</priority>
-  </url>
-
-  <url>
-    <loc>https://spicnspansuffolk.co.uk/services/one-off-cleans</loc>
-    <changefreq>monthly</changefreq>
-    <priority>0.8</priority>
-  </url>
-
-  <url>
-    <loc>https://spicnspansuffolk.co.uk/about</loc>
-    <changefreq>monthly</changefreq>
-    <priority>0.7</priority>
-  </url>
-
-  <url>
-    <loc>https://spicnspansuffolk.co.uk/contact</loc>
-    <changefreq>yearly</changefreq>
-    <priority>0.6</priority>
-  </url>
-
+  @php
+    $pages = [
+        ['route' => 'home', 'changefreq' => 'weekly', 'priority' => '1.0'],
+        ['route' => 'services', 'changefreq' => 'monthly', 'priority' => '0.9'],
+        ['route' => 'services.house-cleaning', 'changefreq' => 'monthly', 'priority' => '0.8'],
+        ['route' => 'services.commercial-cleaning', 'changefreq' => 'monthly', 'priority' => '0.8'],
+        ['route' => 'services.one-off-cleans', 'changefreq' => 'monthly', 'priority' => '0.8'],
+        ['route' => 'about', 'changefreq' => 'monthly', 'priority' => '0.7'],
+        ['route' => 'contact', 'changefreq' => 'yearly', 'priority' => '0.6'],
+    ];
+  @endphp
+  @foreach ($pages as $page)
+    <url>
+      <loc>{{ route($page['route']) }}</loc>
+      <changefreq>{{ $page['changefreq'] }}</changefreq>
+      <priority>{{ $page['priority'] }}</priority>
+    </url>
+  @endforeach
 </urlset>
